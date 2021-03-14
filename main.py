b58b291cbe6d6d50ab7f8a83317f417d82175915
MonkeyInWind <xlyytcy_pyc@outlook.com>
2021-03-14 21:31:05 +0800

    system info

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from fastapi.middleware.cors import CORSMiddleware;
 from fastapi.websockets import WebSocket;
 from  routes import routerTest;
 from utils.user_input import user_target;
+import platform;
 
 app = FastAPI()
 
@@ -16,147 +17,153 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(routerTest.router, prefix='/api')
-
-@app.get('/')
-async def hello_world():
-    return {
-        'mess': 'hello world'
-    }
-
-@app.get('/test/a')
-async def testA():
-    return 'this is a'
-
-@app.get('/test/{id}')
-async def test1(id: int):
-    return {
-        'id': id
-    }
-
-# 放这会先匹配到'/test/{id}'
+# app.include_router(routerTest.router, prefix='/api')
+#
+# @app.get('/')
+# async def hello_world():
+#     return {
+#         'mess': 'hello world'
+#     }
+#
 # @app.get('/test/a')
 # async def testA():
 #     return 'this is a'
+#
+# @app.get('/test/{id}')
+# async def test1(id: int):
+#     return {
+#         'id': id
+#     }
+#
+# # 放这会先匹配到'/test/{id}'
+# # @app.get('/test/a')
+# # async def testA():
+# #     return 'this is a'
+#
+# # 枚举
+# class Person(str, Enum):
+#     name = 'sb'
+#     age = 12
+#
+# @app.get('/person/{name}')
+# async def get_person(name: Person):
+#     print(name.value, name)
+#     if name == Person.name:
+#         return name
+#     return 'not found'
+#
+# @app.get('/query')
+# async def query(a: int = 0, b: int = 1):
+#     return [a, b]
+#
+# @app.get('/query_bool/{a}')
+# async def query_bool(a: str, bool: bool):
+#     return {
+#         'a': a,
+#         'bool': bool
+#     }
+#
+# class Animal(BaseModel):
+#     name: str
+#     age: int
+#     color: str = None
+#     index: int = None
+#
+# # body
+# @app.post('/animal')
+# async def get_animal(animal: Animal):
+#     return animal
+#
+# @app.post('/animal_color')
+# async def get_animal_color(animal: Animal):
+#     animal_dict = animal.dict()
+#     animal_dict.update({'color': 'red'})
+#     return animal_dict
+#
+# # query
+# @app.post('/animal_query')
+# async def animal_query(color: str):
+#     return {
+#         'name': 'sb',
+#         'age': 12,
+#         'color': color
+#     }
+#
+# # body + path
+# @app.post('/animal/{index}')
+# async def get_animal_by_index(index: int, animal: Animal):
+#     animal_dict = animal.dict()
+#     animal_dict.update({'index': index, 'color': 'red'})
+#     return animal_dict
+#
+# # body + path +query
+# @app.post('/animal_with_query/{index}')
+# async def get_animal_by_index_and_color(index: int, animal: Animal, color: str):
+#     animal_dict = animal.dict()
+#     animal_dict.update({
+#         'index': index,
+#         'color': color
+#     })
+#     return animal_dict
+#
+# # Query
+# @app.get('/test_query')
+# async def test_query(q: str = Query(None, max_length = 2)):
+#     res = {
+#         'a': 1,
+#         'b': 2
+#     }
+#     if q:
+#         res.update({
+#             'q': q
+#         })
+#     return res
+#
+# # Query with regex
+# @app.get('/query_with_regex')
+# async def query_regex(q: str = Query(None, regex='^abc$')):
+#     res = {
+#         'x': 'a',
+#         'y': 'b'
+#     }
+#     if q:
+#         res.update({
+#             'q': q
+#         })
+#     return res
+#
+# # Body
+# @app.post('/test_body')
+# async def test_body(data = Body(...)):
+#     return data
+#
+# # cookie & header
+# @app.post('/test_cookie_header')
+# async def test_cookie_and_header(data = Body(...), lan: str = Cookie(None), agent: str = Header(None)):
+#     return {
+#         'lan': lan,
+#         'agent': agent,
+#         'data': data
+#     }
+#
+# # http status
+# @app.get('/http_status', status_code = status.HTTP_201_CREATED)
+# async def test_http_status():
+#     return 'status: 201'
 
-# 枚举
-class Person(str, Enum):
-    name = 'sb'
-    age = 12
 
-@app.get('/person/{name}')
-async def get_person(name: Person):
-    print(name.value, name)
-    if name == Person.name:
-        return name
-    return 'not found'
-
-@app.get('/query')
-async def query(a: int = 0, b: int = 1):
-    return [a, b]
-
-@app.get('/query_bool/{a}')
-async def query_bool(a: str, bool: bool):
-    return {
-        'a': a,
-        'bool': bool
-    }
-
-class Animal(BaseModel):
-    name: str
-    age: int
-    color: str = None
-    index: int = None
-
-# body
-@app.post('/animal')
-async def get_animal(animal: Animal):
-    return animal
-
-@app.post('/animal_color')
-async def get_animal_color(animal: Animal):
-    animal_dict = animal.dict()
-    animal_dict.update({'color': 'red'})
-    return animal_dict
-
-# query
-@app.post('/animal_query')
-async def animal_query(color: str):
-    return {
-        'name': 'sb',
-        'age': 12,
-        'color': color
-    }
-
-# body + path
-@app.post('/animal/{index}')
-async def get_animal_by_index(index: int, animal: Animal):
-    animal_dict = animal.dict()
-    animal_dict.update({'index': index, 'color': 'red'})
-    return animal_dict
-
-# body + path +query
-@app.post('/animal_with_query/{index}')
-async def get_animal_by_index_and_color(index: int, animal: Animal, color: str):
-    animal_dict = animal.dict()
-    animal_dict.update({
-        'index': index,
-        'color': color
-    })
-    return animal_dict
-
-# Query
-@app.get('/test_query')
-async def test_query(q: str = Query(None, max_length = 2)):
-    res = {
-        'a': 1,
-        'b': 2
-    }
-    if q:
-        res.update({
-            'q': q
-        })
-    return res
-
-# Query with regex
-@app.get('/query_with_regex')
-async def query_regex(q: str = Query(None, regex='^abc$')):
-    res = {
-        'x': 'a',
-        'y': 'b'
-    }
-    if q:
-        res.update({
-            'q': q
-        })
-    return res
-
-# Body
-@app.post('/test_body')
-async def test_body(data = Body(...)):
-    return data
-
-# cookie & header
-@app.post('/test_cookie_header')
-async def test_cookie_and_header(data = Body(...), lan: str = Cookie(None), agent: str = Header(None)):
-    return {
-        'lan': lan,
-        'agent': agent,
-        'data': data
-    }
-
-# http status
-@app.get('/http_status', status_code = status.HTTP_201_CREATED)
-async def test_http_status():
-    return 'status: 201'
+def get_sys_info():
+    sys_info = platform.uname()._asdict();
+    sys_info['platform'] = platform.platform();
+    print(sys_info);
+    return sys_info;
 
 # websocket
 @app.websocket_route('/ws')
 async def ws(websocket: WebSocket):
-    await websocket.accept()
-    await websocket.send_json({
-        'msg': 'ws msg'
-    })
+    sys_info = get_sys_info();
+    await websocket.accept();
+    await websocket.send_json(sys_info);
     while True:
         data = await websocket.receive_text();
         user_target(data);
